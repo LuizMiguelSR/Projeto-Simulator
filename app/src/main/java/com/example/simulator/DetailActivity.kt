25e4528b60b57afb32811f9e74a88f6112e6d54b
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-04-05 19:21:51 -0300

    Criando a Detail Activity

---
+++ app/src/main/java/com/example/simulator/DetailActivity.kt
@@ -0,0 +1,16 @@
+package com.example.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
